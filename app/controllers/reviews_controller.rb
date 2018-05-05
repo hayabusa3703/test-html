30b388e9cf8aaa559b4869f1e6394f4dc3644af7
hayabusa3703 <umibudou3703@gmail.com>
2018-05-05 18:32:34 +0900

    step4 sign_outクリエイト

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,7 @@
 class ReviewsController < RankingController
+
+  before_action :authenticate_user!, only: :new
+
   def new
     @product = Product.find(params[:product_id])
     @review = Review.new
