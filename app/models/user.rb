d3b1883b0c8db19beb4a943f2d43c4763b06f2e4
hayabusa3703 <umibudou3703@gmail.com>
2018-05-06 14:10:13 +0900

    step5_paperclip

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_attached_file :avatar,
+                      styles: { medium: "300×300#", thumb: "100×100#{}"}
+  validates_attachment_content_type :avatar,
+                                      content_type: ["image/jpg","image/jpeg","image_png"]
+
 end
