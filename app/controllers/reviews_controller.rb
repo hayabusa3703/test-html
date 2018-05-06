9b4b8a812194ae9b09f5400b07243a438d6342e6
hayabusa3703 <umibudou3703@gmail.com>
2018-05-06 15:06:41 +0900

    step7 アソシエーション

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -16,6 +16,6 @@ class ReviewsController < RankingController
 
   private
   def create_params
-    params.require(:review).permit(:rate, :review, :nickname).merge(product_id: params[:product_id])
+    params.require(:review).permit(:rate, :review).merge(product_id: params[:product_id], user_id: current_user.id)
   end
 end
